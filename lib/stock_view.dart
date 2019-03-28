283907dc7ba523366614d7e9d130ebccb5763761
Lu Shuang <lushuang@foundersc.com>
2019-03-28 10:21:07 +0800

    move candle-stick out stock view

--- lib/stock_view.dart
+++ lib/stock_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'stock_model.dart';
-import 'stock_painter.dart';
+import 'candlestick_view.dart';
 
 class StockViewPage extends StatelessWidget {
   @override
@@ -18,102 +16,5 @@ class StockViewPage extends StatelessWidget {
     );
   }
 
-}
-
-class CandleSticksView extends StatefulWidget {
-  final String symbol;
-  final Size viewSize;
-
-  CandleSticksView(this.symbol, this.viewSize);
-
-  @override
-  State<CandleSticksView> createState() {
-    return _CandleStickViewState("stocks/00700/k_day.json");
-  }
-
-}
-
-class _CandleStickViewState extends State<CandleSticksView> {
-  String _dataPath;
-  StockData _data;
-  CandleSticksPainterConfig _painterConfig;
-
-  double _virtualWidth;
-
-  _CandleStickViewState(this._dataPath);
-
-  void _onDataReady(StockData data) {
-    setState(() {
-      _data = data;
-      _painterConfig = CandleSticksPainterConfig();
-      _virtualWidth = (_painterConfig.candleWidth + _painterConfig.candleMargin * 2) * data.items.length;
-    });
-  }
-
-  void _onDragUpdate(double offset) {
-    debugPrint(offset.toString());
-    setState(() {
-      double newOffset = _painterConfig.viewOffset - offset;
-
-      if (newOffset < 0) {
-        newOffset = 0;
-      }
-
-      if (newOffset > _virtualWidth - widget.viewSize.width) {
-        newOffset = _virtualWidth - widget.viewSize.width;
-      }
-
-      _painterConfig.viewOffset = newOffset;
-    });
-  }
-
-  @override
-  void initState() {
-    StockData.loadFromBundle(rootBundle, _dataPath).then(
-      (data) { _onDataReady(data); }
-    );
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_data == null) {
-      return ConstrainedBox (
-        constraints: BoxConstraints.loose(
-          Size(double.infinity, 500),
-        ),
-        child: CircularProgressIndicator(),
-      );
-    } else {
-      return ConstrainedBox (
-        constraints: BoxConstraints.loose(
-          Size(double.infinity, 500),
-        ),
-        child: GestureDetector(
-          child: CustomPaint(
-            painter: CandleStickPainter(_data, _painterConfig),
-            size: widget.viewSize,
-          ),
-
-          onHorizontalDragStart: (DragStartDetails details) => {
-            debugPrint("Drag Start: {details}")
-          },
-          onHorizontalDragUpdate: (DragUpdateDetails details) => {
-            _onDragUpdate(details.primaryDelta)
-          },
-          onHorizontalDragEnd: (DragEndDetails details) => {
-            debugPrint("Drag End: {details}")
-          },
-          onHorizontalDragCancel: () => {
-            debugPrint("Drag Canceled")
-          },
-          onHorizontalDragDown: (DragDownDetails details) => {
-            debugPrint("Drag Down: {details}")
-          },
-        ),
-      );
-    }
-  }
 }
 
